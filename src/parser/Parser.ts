af78aa531245c06e61cbd1e32859f2b4c0593ff6
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-08-31 09:41:48 +0100

    Push progress remote

---
+++ src/parser/Parser.ts
@@ -0,0 +1,126 @@
+import { BinaryExpr, Expr, GroupingExpr, LiteralExpr, UnaryExpr } from "../ast/Expr";
+import { Token } from "../ast/Token";
+import { TokenType } from "../ast/TokenType";
+import { Errors } from "../utils/Errors";
+import { SyntaxError } from "./SyntaxError";
+
+export class Parser {
+  private tokens: Token[];
+  private errors: SyntaxError[] = [];
+  private current = 0;
+
+  constructor(tokens: Token[]) {
+    this.tokens = tokens;
+  }
+
+  private expression(): Expr {
+    return this.equality();
+  }
+
+  private equality(): Expr {
+    let expr = this.comparison();
+
+    while (this.match("BANG_EQUAL", "EQUAL_EQUAL")) {
+      const operator = this.previous();
+      const right = this.comparison();
+
+      expr = new BinaryExpr(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private comparison(): Expr {
+    let expr = this.term();
+
+    while (this.match("GREATER", "GREATER_EQUAL", "LESS", "LESS_EQUAL")) {
+      const operator = this.previous();
+      const right = this.term();
+      expr = new BinaryExpr(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private term(): Expr {
+    let expr = this.factor();
+
+    while (this.match("MINUS", "PLUS")) {
+      const operator = this.previous();
+      const right = this.factor();
+      expr = new BinaryExpr(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private factor(): Expr {
+    let expr = this.unary();
+
+    while (this.match("SLASH", "STAR")) {
+      const operator = this.previous();
+      const right = this.unary();
+      expr = new BinaryExpr(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private unary(): Expr {
+    if (this.match("BANG", "MINUS")) {
+      const operator = this.previous();
+      const right = this.unary();
+      return new UnaryExpr(operator, right);
+    }
+
+    return this.primary();
+  }
+
+  private primary(): Expr {
+    if (this.match("NUMBER", "STRING", "FALSE", "TRUE", "NULL")) {
+      const token = this.previous();
+      return new LiteralExpr(token.literal!, token);
+    }
+
+    if (this.match("LEFT_PAREN")) {
+      const expr = this.expression();
+      this.consume("RIGHT_PAREN", Errors.ExpectedRightParen);
+      return new GroupingExpr(expr);
+    }
+
+    throw this.error(this.peek(), Errors.ExpectedExpression);
+  }
+
+  private match(...types: TokenType[]): boolean {
+    for (const type of types) {
+      if (this.check(type)) {
+        this.advance();
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private check(type: TokenType): boolean {
+    if (this.isAtEnd()) return false;
+    return this.peek().type === type;
+  }
+
+  private advance(): Token {
+    if (!this.isAtEnd()) this.current += 1;
+    return this.previous();
+  }
+
+  private isAtEnd(): boolean {
+    return this.peek().type === "EOF";
+  }
+
+  private peek(): Token {
+    return this.tokens[this.current];
+  }
+
+  private previous(): Token {
+    return this.tokens[this.current - 1];
+  }
+}
--- src/utils/Errors.ts
+++ src/utils/Errors.ts
@@ -1,4 +1,6 @@
 export const Errors = {
   UnterminatedString: "unterminated string",
   UnexpectedCharacter: "unexpected character",
+  ExpectedRightParen: "expected right parentheses",
+  ExpectedExpression: "expected expression",
 };
