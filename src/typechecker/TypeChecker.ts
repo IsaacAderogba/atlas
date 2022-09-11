25fd02fc1bbc6e41939bef33e29041ed4a144313
Isaac Aderogba <isaacaderogba1@gmail.com>
2022-09-11 09:44:01 +0100

    Implement typechecker interface

---
+++ src/typechecker/TypeChecker.ts
@@ -0,0 +1,134 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  AssignExpr,
+  BinaryExpr,
+  CallExpr,
+  ExprVisitor,
+  FunctionExpr,
+  GetExpr,
+  GroupingExpr,
+  ListExpr,
+  LiteralExpr,
+  LogicalExpr,
+  RecordExpr,
+  SetExpr,
+  TernaryExpr,
+  ThisExpr,
+  UnaryExpr,
+  VariableExpr,
+} from "../ast/Expr";
+import {
+  BlockStmt,
+  BreakStmt,
+  ClassStmt,
+  ContinueStmt,
+  ErrorStmt,
+  ExpressionStmt,
+  IfStmt,
+  ReturnStmt,
+  StmtVisitor,
+  VarStmt,
+  WhileStmt,
+} from "../ast/Stmt";
+
+export class TypeChecker implements ExprVisitor<void>, StmtVisitor<void> {
+  visitBlockStmt(_stmt: BlockStmt): void {
+    throw new Error("Method not implemented.");
+  }
+  
+  visitBreakStmt(_stmt: BreakStmt): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitClassStmt(_stmt: ClassStmt): void {
+    throw new Error("Method not implemented.");
+  }
+  
+  visitContinueStmt(_stmt: ContinueStmt): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitErrorStmt?(_stmt: ErrorStmt): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitExpressionStmt(_stmt: ExpressionStmt): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitIfStmt(_stmt: IfStmt): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitReturnStmt(_stmt: ReturnStmt): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitVarStmt(_stmt: VarStmt): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitWhileStmt(_stmt: WhileStmt): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitAssignExpr(_expr: AssignExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitBinaryExpr(_expr: BinaryExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitCallExpr(_expr: CallExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitFunctionExpr(_expr: FunctionExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitGetExpr(_expr: GetExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitTernaryExpr(_expr: TernaryExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitGroupingExpr(_expr: GroupingExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitLiteralExpr(_expr: LiteralExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitListExpr(_expr: ListExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitLogicalExpr(_expr: LogicalExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitRecordExpr(_expr: RecordExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitSetExpr(_expr: SetExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitThisExpr(_expr: ThisExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitUnaryExpr(_expr: UnaryExpr): void {
+    throw new Error("Method not implemented.");
+  }
+
+  visitVariableExpr(_expr: VariableExpr): void {
+    throw new Error("Method not implemented.");
+  }
+}
